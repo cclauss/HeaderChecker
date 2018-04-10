dfd208c3c7f708bb4799657a6b6c1b4d42f5b752
Nikhil Aryal <naryal2580@gmail.com>
2018-04-10 19:28:27 +0530

    Update vrDisVlnCkr.py
    Cool Changes

--- vrDisVlnCkr.py
+++ vrDisVlnCkr.py
@@ -8,26 +8,81 @@ from urlparse import urlparse
 import httplib, sys
 from requests.packages.urllib3 import disable_warnings as dsable_wrng
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from os import system as stm
+from sys import exit as x
 
 dsable_wrng(InsecureRequestWarning)
 adgt = list(dgtz)
 hdrz = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0', 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Connection':'keep-alive',}
 
-def check_url(url):
-  url = urlparse(url)
-  conn = httplib.HTTPConnection(url.netloc)
-  conn.request("HEAD", url.path)
-  if conn.getresponse():
-    return True
-  else:
-    return False
-
 try:
+	def check_url(url):
+		url = urlparse(url)
+		conn = httplib.HTTPConnection(url.netloc)
+		conn.request("HEAD", url.path)
+		if conn.getresponse():
+			return True
+		else:
+			return False
+
+	def c_s():
+		s = hdrz['Server']
+		for i in adgt:
+			if i in s:
+				print 'Server:', s
+				break
+			else:
+				print '\nWednesdayThursdayFriday... See the headers up ⤊ 😜'
+				break
+
+	def c_pb():
+		xpb = hdrz['X-Powered-By']
+		print 'X-Powered-By:', xpb
+
+	def c_av():
+		xanv = hdrz['X-AspNet-Version']
+		for i in adgt:
+			if i in xanv:
+				print 'X-AspNet-Version:', xanv
+				break
+			else:
+				break
+
+	def c_rp():
+		rp = hdrz['Referrer-Policy']
+		if rp == 'strict-origin-when-cross-origin': pass
+		else: print 'Referrer-Policy:', rp
+
+	def c_cto():
+		xcto = hdrz['X-Content-Type-Options']
+		if xcto == 'nosniff': pass
+		else: print 'X-Content-Type-Options:', xcto
+
+	def c_xp():
+		xxp = hdrz['X-Xss-Protection']
+		if xxp == '1; mode=block': pass
+		else: print 'X-Xss-Protection:', xxp
+
+	def c_fo():
+		xfo = hdrz['X-Frame-Options']
+		if xfo == 'SAMEORIGIN' or xfo == 'DENY': pass
+		else: print 'X-Frame-Options:', xfo
+
+	def c_csp():
+		csp = hdrz['Content-Security-Policy']
+		print 'Content-Security-Policy:', csp
+
+	def c_sts():
+		sts = hdrz['Strict-Transport-Security']
+		if 'max-age' in sts: pass
+		else: print 'Strict-Transport-Security:', sts
+
 	if len(arg) != 2:
 		inpt = raw_input('Enter the URL: ')
 	else:
 		inpt = arg[1]
-	print 'Checking URL...'
+	print '\033[H\033[J'
+	print 'Checking URL: %s'%(inpt)
 	if 'http://' in inpt:
 		if check_url(inpt):
 			url = inpt
@@ -43,6 +98,7 @@ try:
 			url = url_https
 		else:
 			print 'Whatttttttt...'
+	print 'Valid URL!'
 	print '\nREQUEST HEADERS\n'
 	for k in hdrz: print k, ':', hdrz[k]
 	r = reg(url, headers=hdrz, verify=False)
@@ -50,84 +106,26 @@ try:
 	kyz = hdrz.keys()
 	print '\nRESPONSE HEADERS\n'
 	for k in hdrz: print k, ':', hdrz[k]
-	if 'Server' in kyz and 'X-Powered-By' in kyz and 'X-AspNet-Version' in kyz:
+	if 'Server' in kyz or 'X-Powered-By' in kyz or 'X-AspNet-Version' in kyz or 'Referrer-Policy' in kyz or 'X-Content-Type-Options' in kyz or 'X-Xss-Protection' in kyz or 'X-Frame-Options' in kyz or 'Content-Security-Policy' in kyz or 'Strict-Transport-Security' in kyz:
 		print '\nVULNERABILITIES\n'
-		s = hdrz['Server']
-		for i in adgt:
-			if i in s:
-				print 'Server:', s
-				break
-			else:
-				break
-
-		xpb = hdrz['X-Powered-By']
-		print 'X-Powered-By:', xpb
-
-		xanv = hdrz['X-AspNet-Version']
-		for i in adgt:
-			if i in xanv:
-				print 'X-AspNet-Version:', xanv
-				break
-			else:
-				break
-
-	elif 'Server' in kyz and 'X-Powered-By' in kyz:
-		print '\nVULNERABILITIES\n'
-		s = hdrz['Server']
-		for i in adgt:
-			if i in s:
-				print 'Server:', s
-				break
-			else:
-				break
-
-		xpb = hdrz['X-Powered-By']
-		print 'X-Powered-By:', xpb
-
-	elif 'Server' in kyz and 'X-AspNet-Version' in kyz:
-		print '\nVULNERABILITIES\n'
-		s = hdrz['Server']
-		for i in adgt:
-			if i in s:
-				print 'Server:', s
-				break
-			else:
-				break
-
-		xanv = hdrz['X-AspNet-Version']
-		for i in adgt:
-			if i in xanv:
-				print 'X-AspNet-Version:', xanv
-				break
-			else:
-				break
-
-	elif 'X-Powered-By' in kyz and 'X-AspNet-Version' in kyz:
-		print '\nVULNERABILITIES\n'
-		xpb = hdrz['X-Powered-By']
-		print 'X-Powered-By:', xpb
-
-		xanv = hdrz['X-AspNet-Version']
-		for i in adgt:
-			if i in xanv:
-				print 'X-AspNet-Version:', xanv
-				break
-			else:
-				break
-	elif 'X-Powered-By' in kyz:
-		print '\nVULNERABILITIES\n'
-		xpb = hdrz['X-Powered-By']
-		print 'X-Powered-By:', xpb
-	elif 'X-AspNet-Version' in kyz:
-		print '\nVULNERABILITIES\n'
-		xanv = hdrz['X-AspNet-Version']
-		for i in adgt:
-			if i in xanv:
-				print 'X-AspNet-Version:', xanv
-				break
-			else:
-				break
-	else:
-		print '\nWednesdayThursdayFriday... See the headers up ⤊ 😜'
+		if 'Server' in kyz: c_s()
+		else: pass
+		if 'X-Powered-By' in kyz: c_pb()
+		else: pass
+		if 'X-AspNet-Version' in kyz: c_av()
+		else: pass
+		if 'Referrer-Policy' in kyz: c_rp()
+		else: print 'No Referrer-Policy Header...'
+		if 'X-Content-Type-Options' in kyz: c_cto()
+		else: print 'No X-Content-Type-Options Header...'
+		if 'X-Xss-Protection' in kyz: c_xp()
+		else: print 'No X-Xss-Protection Header...'
+		if 'X-Frame-Options' in kyz: c_fo()
+		else: print 'No X-Frame-Options Header...'
+		# if 'Content-Security-Policy' in kyz: c_csp()
+		# else: print 'No Content-Security-Policy Header...'
+		if 'Strict-Transport-Security' in kyz: c_sts()
+		else: print 'NO Strict-Transport-Security Header...'
+	else: print 'WednesdayThursdayFriday... See the headers up ⤊ 😜'
+	stm('gnome-screenshot -w')
 except KeyboardInterrupt: print '\b\bInterruption from Keyboard...'
-except rex.ConnectionError: print 'Is it https??'
